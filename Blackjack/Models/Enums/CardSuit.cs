515db4b0aff1980e785dd44c2414a7b74c43f1ba
Svetoslav.Yordanov <shapeshifter003@gmail.com>
2021-02-02 19:36:43 +0200

    initial commit

---
+++ Blackjack/Models/Enums/CardSuit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blackjack.Models.Enums
+{
+    public enum CardSuit
+    {
+        Hearts,
+        Clubs,
+        Diamonds,
+        Spades
+    }
+}
